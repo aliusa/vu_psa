03914d821438dc2a13a4e6f398c98f6419407eba
alius <alius.s@kryptis.lt>
2025-04-12 22:13:10 +0300

    unit tests

--- tests/WebTestCase/FrontControllerTest.php
+++ tests/WebTestCase/FrontControllerTest.php
@@ -6,12 +6,41 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class FrontControllerTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testHomepage(): void
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello World');
+        $this->assertSelectorExists('a[href^="/services/"]');
+    }
+
+    public function testLoginPage(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/users/login');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists('form h1');
+        $this->assertSelectorExists('form #email');
+        $this->assertSelectorExists('form #password');
+        $this->assertSelectorExists('form button[type=submit]');
+        $this->assertSelectorExists('form input[name="_csrf_token"]');
+    }
+
+    public function testLogoutPage(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/users/logout');
+
+        $this->assertResponseRedirects('/users/login');
+    }
+
+    public function testServices(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/services');
+
+        $this->assertResponseIsSuccessful();
     }
 }
